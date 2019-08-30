f4b6a26f9264028aed7df6fffe5cf49d7354a64b
Vitaly Zaitsev <vitaly@easycoding.org>
2019-08-30 11:12:35 +0200

    Updated FAQ contents.

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2019.07.31',
+    VERSION: '2019.08.30',
     LANGUAGE: 'ru',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
