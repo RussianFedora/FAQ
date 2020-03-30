61d5fa1a3bac3c32494a3bde15d9187f203e82c4
Vitaly Zaitsev <vitaly@easycoding.org>
2020-03-30 12:45:34 +0200

    Incremented release version to 2020.03.30.

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,7 @@ from os import getenv
 from time import strftime
 
 # Configuring version and Git snapshots.
-rel_version = '2020.02.29'
+rel_version = '2020.03.30'
 rel_snapshot = getenv('CI_HASH')
 
 # -- General configuration ------------------------------------------------
