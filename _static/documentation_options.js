e3bd719ecf10a3c6ca6965b568306c10533afcb3
xvitaly <vitaly@easycoding.org>
2021-09-30 16:34:51 +0000

    Deploying to gh-pages from @ RussianFedora/FAQ@c01667dd41dc12c7f466dfacb819b7a2ceaf7f62 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2021.09.25',
+    VERSION: '2021.09.30',
     LANGUAGE: 'ru',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
