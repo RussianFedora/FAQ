bf9f97df206b36489a08ebc8f40c317003bd28df
sotnikov123 <sotnikov123@yandex.ru>
2022-01-04 17:44:26 +0000

    Deploying to gh-pages from @ sotnikov123/FAQ@52c86cbae5f90c5bc6c9ddf93b6994bd08c81aab 🚀

---
+++ _static/search.js
@@ -0,0 +1,12 @@
+/**
+ * SPDX-FileCopyrightText: 2018-2021 EasyCoding Team and contributors
+ *
+ * SPDX-License-Identifier: CC-BY-SA-4.0
+*/
+
+docsearch({
+    apiKey: '67605489a264a21d2328a7f51a2fefc4',
+    indexName: 'russianfedora',
+    inputSelector: '#searchbox input[type="text"]',
+    debug: false
+});
